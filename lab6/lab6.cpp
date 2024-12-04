11f68e88d4e6c3782c09cf8401fdcc36cd5c05ea
egor <ef188209@gmail.com>
2024-12-04 12:41:27 +0300

    lab6

---
+++ lab6/lab6.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int size, a = 0;
+    cout << "Size string: ";
+    cin >> size;
+    cin.ignore();
+
+    char *stroka = new char[size + 1];
+    char *rez = new char[size + 1];
+    cout << "Enter string: ";
+    cin.getline(stroka, size + 1);
+
+    int numWord = 0;
+    bool inWord = false;
+
+    for (int i = 0; stroka[i] != '\0'; i++)
+    {
+        if (stroka[i] != ' ' && !inWord)
+        {
+            inWord = true;
+            numWord++;
+        }
+
+        if (inWord && (numWord == 1 || numWord == 3 || numWord == 5))
+        {
+            rez[a] = stroka[i];
+            a++;
+        }
+
+        if (stroka[i] == ' ' && inWord)
+        {
+            inWord = false;
+            // if (numWord == 1 || numWord == 3 || numWord == 5)
+            // {
+            //     rez[a] = ' ';
+            //     a++;
+            // }
+        }
+    }
+    =
+
+        if (inWord && (numWord == 1 || numWord == 3 || numWord == 5))
+    {
+        rez[a] = ' ';
+        a++;
+    }
+
+    rez[a - 1] = '\0';
+
+    cout << "rez: " << rez << endl;
+
+    delete[] stroka;
+    delete[] rez;
+
+    return 0;
+}
